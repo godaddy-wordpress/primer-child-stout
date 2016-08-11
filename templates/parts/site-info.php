9aa7bc5b2fe4ae8d7f742dcce50c918d2875d147
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-08-11 17:04:36 -0400

    Add colon

--- templates/parts/site-info.php
+++ templates/parts/site-info.php
@@ -14,7 +14,7 @@
 
 			<?php get_template_part( 'templates/parts/footer-navigation' ); ?>
 
-			<?php printf( _x( 'Built with %1$s by %2$s.', '1. theme name link, 2. theme author link', 'primer' ), '<a href="https://wordpress.org/themes/primer/" rel="designer">Primer</a>', '<a href="https://www.godaddy.com/" rel="designer">GoDaddy</a>' ) ?>
+			<?php printf( _x( 'Built with %1$s by %2$s.', '1. theme name link, 2. theme author link', 'primer' ), '<a href="https://wordpress.org/themes/primer/" rel="designer">Primer</a>', '<a href="https://www.godaddy.com/" rel="designer">GoDaddy</a>' ); ?>
 
 		</div><!-- .site-info-text -->
 
